a896eb331aec81a533c053eaf779d515b2601210
hokimyat-stack <hokimyatgps@gmail.com>
2026-08-10 16:18:39 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 // src/config.js
-export const API_URL = 'https://script.google.com/macros/s/AKfycbwBm3VN3DfQD7Tj71zG-opItPnsEqBpgRwv8FVWTTRK2yj6u52v15ldBADTaaXkSBk/exec';
-export const KALIT = '7Kf92mQ';
-export const GITHUB_RELEASES = 'https://api.github.com/repos/hokimyat-stack/hisobot-ilova/releases/latest';
+export const API_URL = 'https://xisobot-api.onrender.com/api'; // Yangi Render backend manzili
+export const GITHUB_RELEASES = 'https://api.github.com/repos/hokimyat-stack/xisobot-ilova/releases/latest';
 export const ILOVA_VERSIYA = '3.0.0';
 export const RANG = {
   asosiy: '#1758B8', toq: '#17212B', fon: '#F6F8FB', oq: '#FFFFFF',
